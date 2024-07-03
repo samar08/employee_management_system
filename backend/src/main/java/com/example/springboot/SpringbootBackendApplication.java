8270a641885a9aa65d2c1df0f5cf58b0f1703a89
samar08 <samarasimhareddy.mohan@gmail.com>
2024-07-03 10:42:16 +0530

    init commit

---
+++ backend/src/main/java/com/example/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,35 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+//		Employee employee = new Employee();
+//		employee.setFirstName("Ramesh");
+//		employee.setLastName("Fadatare");
+//		employee.setEmailId("ramesh@gmail.com");
+//		employeeRepository.save(employee);
+//
+//		Employee employee1 = new Employee();
+//		employee1.setFirstName("John");
+//		employee1.setLastName("Cena");
+//		employee1.setEmailId("cena@gmail.com");
+//		employeeRepository.save(employee1);
+	}
+}
---
+++ frontend/src/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <div className = "navbar-brand">
+                            Employee Management Application
+                        </div>    
+                    </div>
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
