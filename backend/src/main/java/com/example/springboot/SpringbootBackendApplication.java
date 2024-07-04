914e2c98d3ae37b00d45ed651695d43ddf00b46f
samar08 <samarasimhareddy.mohan@gmail.com>
2024-07-04 11:48:53 +0530

    init commit

--- backend/src/main/java/com/example/springboot/SpringbootBackendApplication.java
+++ backend/src/main/java/com/example/springboot/SpringbootBackendApplication.java
@@ -20,16 +20,6 @@ public class SpringbootBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		Employee employee = new Employee();
-//		employee.setFirstName("Ramesh");
-//		employee.setLastName("Fadatare");
-//		employee.setEmailId("ramesh@gmail.com");
-//		employeeRepository.save(employee);
-//
-//		Employee employee1 = new Employee();
-//		employee1.setFirstName("John");
-//		employee1.setLastName("Cena");
-//		employee1.setEmailId("cena@gmail.com");
-//		employeeRepository.save(employee1);
+
 	}
 }
--- backend/src/main/java/com/example/springboot/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/springboot/repository/EmployeeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.springboot.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // all crud database methods
+    
 }
 
 
